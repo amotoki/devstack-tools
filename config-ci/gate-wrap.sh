5d7153b581e4107905108de760e250b257caa65e
Akihiro Motoki <motoki@da.jp.nec.com>
2014-01-14 20:01:10 +0900

    Add gerrit checkout and stack.sh to gate-wrap.sh

--- config-ci/gate-wrap.sh
+++ config-ci/gate-wrap.sh
@@ -3,7 +3,11 @@
 CACHE_URL=http://10.56.45.207/openstack
 PROXY=http://proxygate2.nic.nec.co.jp:8080
 
-WORKDIR=`pwd`
+WORKSPACE=${WORKSPACE:-$(pwd)}
+
+# Set this to the time in minutes that the gate test should be allowed
+# to run before being aborted (default 60).
+export DEVSTACK_GATE_TIMEOUT=${DEVSTACK_GATE_TIMEOUT:-60}
 
 DEST=/opt/stack
 GITTAR=gitrepo.tgz
@@ -20,7 +24,7 @@ for d in *; do
   cd ..
 done
 
-cd $WORKDIR
+cd $WORKSPACE
 PIPTAR=pip.cache.tgz
 http_proxy= wget -O $PIPTAR $CACHE_URL/$PIPTAR
 tar xzf $PIPTAR
@@ -29,3 +33,22 @@ sudo mv pip.cache/* /var/cache/pip
 sudo chown -R root:root /var/cache/pip
 rm -rf pip.cache
 rm -f $PIPTAR
+
+REPO_URL=https://review.openstack.org
+#GERRIT_PROJECT=openstack/neutron
+#GERRIT_REFSPEC=refs/changes/01/66501/1
+cd /opt/stack/$(basename $GERRIT_PROJECT)
+https_proxy=$PROXY git fetch $REPO_URL/$GERRIT_PROJECT $GERRIT_REFSPEC && git checkout FETCH_HEAD
+
+if ! function_exists "gate_hook"; then
+  # the command we use to run the gate
+  function gate_hook {
+    timeout -s 9 ${DEVSTACK_GATE_TIMEOUT}m $BASE/new/devstack-gate/devstack-vm-gate.sh
+  }
+fi
+
+export PYTHONUNBUFFERED=true
+cd /opt/stack/devstack
+cp $WORKSPACE/devstack-tools/config-ci/localrc .
+cat localrc
+./stack.sh
