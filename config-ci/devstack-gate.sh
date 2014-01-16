fae53e1f5913bc21e8681867c7fba9ec4d7da8a4
Akihiro Motoki <motoki@da.jp.nec.com>
2014-01-16 16:46:32 +0900

    Allow to run multiple tempest tests

--- config-ci/devstack-gate.sh
+++ config-ci/devstack-gate.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
 set -o errexit
 
@@ -13,23 +13,33 @@ echo "Running devstack"
 cd $DEST/devstack
 ./stack.sh
 
+# Ensure to run all tests
+set +o errexit
+
+# It is required to pass Swift related tests
 unset http_proxy
 unset https_proxy
 
+res=0
 cd $DEST/tempest
+
 if [ "$DEVSTACK_GATE_SMOKE_SERIAL" -eq "1" ]; then
   echo "Running tempest smoke tests"
   bash tools/pretty_tox_serial.sh '(?!.*\[.*\bslow\b.*\])((smoke)|(^tempest\.scenario)) {posargs}'
+  [[ $? -eq 0 && $res -eq 0 ]]
   res=$?
-elif [ "$DEVSTACK_GATE_NETWORK_API" -eq "1" ]; then
-  echo "Running API tests"
-  bash tools/pretty_tox_serial.sh 'tempest.api.network {posargs}'
-  res=$?
-elif [ "$DEVSTACK_GATE_TEMPEST_SCENARIO" -eq "1" ]; then
+fi
+if [ "$DEVSTACK_GATE_TEMPEST_SCENARIO" -eq "1" ]; then
   echo "Running scenario tests"
   bash tools/pretty_tox_serial.sh 'tempest.scenario {posargs}'
+  [[ $? -eq 0 && $res -eq 0 ]]
+  res=$?
+fi
+if [ "$DEVSTACK_GATE_NETWORK_API" -eq "1" ]; then
+  echo "Running API tests"
+  bash tools/pretty_tox_serial.sh 'tempest.api.network {posargs}'
+  [[ $? -eq 0 && $res -eq 0 ]]
   res=$?
 fi
 
-[[ $res -eq 0 ]]
-exit $?
+exit $res
