974432bc93dbcfa4b62f79ef4062f025c8362876
Akihiro Motoki <motoki@da.jp.nec.com>
2014-01-17 14:51:53 +0900

    Collect logs

--- config-ci/gate-wrap.sh
+++ config-ci/gate-wrap.sh
@@ -4,7 +4,12 @@ CACHE_URL=http://10.56.45.207/openstack
 export PROXY=http://proxygate2.nic.nec.co.jp:8080
 export DEST=/opt/stack
 
+# Useful when runninng this script manually
 WORKSPACE=${WORKSPACE:-$(pwd)}
+
+# Make a directory to store logs
+rm -rf logs
+mkdir -p logs
 
 # Set this to the time in minutes that the gate test should be allowed
 # to run before being aborted (default 60).
@@ -39,6 +44,9 @@ prepare_pipcache() {
 }
 
 fetch_target_patchset() {
+  if [ -z "$GERRIT_PROJECT" ]; then
+    return
+  fi
   REPO_URL=https://review.openstack.org
   #GERRIT_PROJECT=openstack/neutron
   #GERRIT_REFSPEC=refs/changes/01/66501/1
@@ -51,6 +59,104 @@ setup_devstack() {
   cat $DEST/devstack/localrc
 }
 
+setup_host() {
+  # Start with a fresh syslog
+  sudo stop rsyslog
+  sudo mv /var/log/syslog /var/log/syslog-pre-devstack
+  sudo mv /var/log/kern.log /var/log/kern_log-pre-devstack
+  sudo touch /var/log/syslog
+  sudo chown /var/log/syslog --ref /var/log/syslog-pre-devstack
+  sudo chmod /var/log/syslog --ref /var/log/syslog-pre-devstack
+  sudo chmod a+r /var/log/syslog
+  sudo touch /var/log/kern.log
+  sudo chown /var/log/kern.log --ref /var/log/kern_log-pre-devstack
+  sudo chmod /var/log/kern.log --ref /var/log/kern_log-pre-devstack
+  sudo chmod a+r /var/log/kern.log
+  sudo start rsyslog
+}
+
+cleanup_host() {
+  # Enabled detailed logging, since output of this function is redirected
+  #set -o xtrace
+
+  cd $WORKSPACE
+  ls -lR
+
+  # Sleep to give services a chance to flush their log buffers.
+  sleep 2
+
+  sudo cp /var/log/syslog $WORKSPACE/logs/syslog.txt
+  sudo cp /var/log/kern.log $WORKSPACE/logs/kern_log.txt
+  sudo cp /var/log/apache2/horizon_error.log $WORKSPACE/logs/horizon_error.log
+  mkdir $WORKSPACE/logs/rabbitmq/
+  sudo cp /var/log/rabbitmq/* $WORKSPACE/logs/rabbitmq/
+  mkdir $WORKSPACE/logs/sudoers.d/
+
+  sudo cp /etc/sudoers.d/* $WORKSPACE/logs/sudoers.d/
+  sudo cp /etc/sudoers $WORKSPACE/logs/sudoers.txt
+
+  local NEWLOGTARGET=$WORKSPACE/logs
+  local BASE=/opt/stack
+  sudo cp $BASE/logs/screen-* $NEWLOGTARGET/
+  sudo cp $BASE/logs/devstack.txt $NEWLOGTARGET/
+  sudo cp $BASE/devstack/localrc $WORKSPACE/logs/localrc.txt
+
+  sudo iptables-save > $WORKSPACE/logs/iptables.txt
+  df -h> $WORKSPACE/logs/df.txt
+
+  pip freeze > $WORKSPACE/logs/pip-freeze.txt
+
+  # Process testr artifacts.
+  if [ -f $BASE/tempest/.testrepository/0 ]; then
+      sudo cp $BASE/tempest/.testrepository/0 $WORKSPACE/subunit_log.txt
+      #sudo python /usr/local/jenkins/slave_scripts/subunit2html.py $WORKSPACE/subunit_log.txt $WORKSPACE/testr_results.html
+      sudo python $WORKSPACE/devstack-tools/config-ci/subunit2html.py $WORKSPACE/subunit_log.txt $WORKSPACE/testr_results.html
+      sudo gzip -9 $WORKSPACE/subunit_log.txt
+      sudo gzip -9 $WORKSPACE/testr_results.html
+      sudo chown jenkins:jenkins $WORKSPACE/subunit_log.txt.gz $WORKSPACE/testr_results.html.gz
+      sudo chmod a+r $WORKSPACE/subunit_log.txt.gz $WORKSPACE/testr_results.html.gz
+  elif [ -f $BASE/tempest/.testrepository/tmp* ]; then
+      # If testr timed out, collect temp file from testr
+      sudo cp $BASE/tempest/.testrepository/tmp* $WORKSPACE/subunit_log.txt
+      sudo gzip -9 $WORKSPACE/subunit_log.txt
+      sudo chown jenkins:jenkins $WORKSPACE/subunit_log.txt.gz
+      sudo chmod a+r $WORKSPACE/subunit_log.txt.gz
+  fi
+
+  if [ -f $BASE/tempest/tempest.log ] ; then
+      sudo cp $BASE/tempest/tempest.log $WORKSPACE/logs/tempest.log
+  fi
+
+  # Make sure jenkins can read all the logs
+  sudo chown -R jenkins:jenkins $WORKSPACE/logs/
+  sudo chmod a+r $WORKSPACE/logs/
+
+  rename 's/\.log$/.txt/' $WORKSPACE/logs/*
+  rename 's/(.*)/$1.txt/' $WORKSPACE/logs/sudoers.d/*
+  rename 's/\.log$/.txt/' $WORKSPACE/logs/rabbitmq/*
+
+  mv $WORKSPACE/logs/rabbitmq/startup_log \
+     $WORKSPACE/logs/rabbitmq/startup_log.txt
+
+  # Remove duplicate logs
+  rm $WORKSPACE/logs/*.*.txt
+
+  # Compress all text logs
+  find $WORKSPACE/logs -iname '*.txt' -execdir gzip -9 {} \+
+  find $WORKSPACE/logs -iname '*.dat' -execdir gzip -9 {} \+
+
+  # Save the tempest nosetests results
+  sudo cp $BASE/tempest/nosetests*.xml $WORKSPACE/
+  sudo chown jenkins:jenkins $WORKSPACE/nosetests*.xml
+  sudo chmod a+r $WORKSPACE/nosetests*.xml
+
+  ls -lR
+
+  # Disable detailed logging as we return to the main script
+  #set +o xtrace
+}
+
+setup_host
 if [ ! -n "$SKIP" ]; then
   prepare_gitrepo
   prepare_pipcache
@@ -75,5 +181,7 @@ GATEPID=`cat $WORKSPACE/gate.pid`
     echo "Killing process group ${GATEPID}"
     sudo kill -s 9 -${GATEPID}
 fi
+
+cleanup_host
 
 exit $RETVAL
